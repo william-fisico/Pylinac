57f5419d0ce1d9426f7c15db095a4219c1d7c0fb
William Santos <william.wapsantos@gmail.com>
2020-07-02 09:42:54 -0300

    Primeiro teste

---
+++ demo_PicketFence.py
@@ -0,0 +1,10 @@
+###########################################
+# William A. P. dos Santos                #
+# william.wapsantos@gmail.com             #
+# Testanto Pylinac                        #
+# 02 de julho de 2020                     #
+###########################################
+
+from pylinac import PicketFence
+
+PicketFence.run_demo()
