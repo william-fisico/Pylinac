5d4cda8f7ef867ffe3d76916074b5f0f6179fb28
William Santos <william.wapsantos@gmail.com>
2020-07-02 12:04:20 -0300

    Teste Relatorio

--- demo_PicketFence.py
+++ demo_PicketFence.py
@@ -7,4 +7,13 @@
 
 from pylinac import PicketFence
 
-PicketFence.run_demo()
+
+#PicketFence.run_demo()
+pf = PicketFence.from_demo_image()
+pf.analyze(tolerance=0.05, action_tolerance=0.03)
+# print results to the console
+print(pf.results())
+# view analyzed image
+pf.plot_analyzed_image()
+# saver PDF report
+#pf.publish_pdf(filename='Demo Picket Fence.pdf')
