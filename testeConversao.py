4f3cb983e225bb072ef3e23c91a0d29690fd3510
William Santos - Notebook Samsung <william.wapsantos@gmail.com>
2020-07-05 21:05:17 -0300

    Modulo para conversao em Dicom

--- testeConversao.py
+++ testeConversao.py
@@ -12,3 +12,11 @@ plt.imshow(teste.pixel_array)
 plt.show()
 
 ConvertToDicom.save_dicom('teste_modulo.dcm')
+
+'''
+Bibliografia modulos
+https://stackabuse.com/creating-and-importing-modules-in-python/
+https://docs.python.org/3/tutorial/classes.html
+https://www.w3schools.com/python/python_classes.asp
+
+'''
